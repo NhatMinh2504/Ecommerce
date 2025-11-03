3559664229200bc92710128b612c74c3db7dcceb
minhpn2004 <tp029223@gmail.com>
2025-11-03 18:54:27 +0700

    Add menu, fix and load name category in  menu

--- EcommerceMVC/obj/Debug/net8.0/EcommerceMVC.AssemblyInfo.cs
+++ EcommerceMVC/obj/Debug/net8.0/EcommerceMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5a124c4996ddae700b4e4034298233834d54316")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ca638911e3af11994ef851511f15804a9fae682")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
