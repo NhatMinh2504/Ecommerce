44e73d35899a6924a78d9f28a2c7ff01563df6cb
minhpn2004 <tp029223@gmail.com>
2025-11-05 01:31:16 +0700

    Complete Search In Shop MenuLoai

---
+++ EcommerceMVC/Controllers/HangHoaController.cs
@@ -0,0 +1,51 @@
+﻿using EcommerceMVC.Data;
+using EcommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+
+namespace EcommerceMVC.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        private readonly Hshop2023Context db;
+        public HangHoaController(Hshop2023Context context) {
+        db = context;
+        }
+        public IActionResult Index(int? loai)
+        {
+            var hangHoas=db.HangHoas.AsQueryable();
+            if (loai.HasValue)
+            {
+                hangHoas = hangHoas.Where(hh => hh.MaLoai == loai.Value);
+            }
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHh = p.TenHh,
+                Hinh = p.Hinh ?? "",
+                DonGia = p.DonGia ?? 0,
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
+        }
+        public IActionResult Search(string? query)
+        {
+            var hangHoas = db.HangHoas.AsQueryable();
+            if (query!=null)
+            {
+                hangHoas = hangHoas.Where(hh => hh.TenHh.Contains(query));
+            }
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHh = p.TenHh,
+                Hinh = p.Hinh ?? "",
+                DonGia = p.DonGia ?? 0,
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
+        }
+    }
+}
--- EcommerceMVC/obj/Debug/net8.0/EcommerceMVC.AssemblyInfo.cs
+++ EcommerceMVC/obj/Debug/net8.0/EcommerceMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ca638911e3af11994ef851511f15804a9fae682")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3559664229200bc92710128b612c74c3db7dcceb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
